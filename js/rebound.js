7927c2ab3e842fd73ce31de972a8d11f9d257c83
Shankar Ghimire <ssghimire@gmail.com>
2020-08-24 21:32:21 -0400

    Finishes the basic game version

--- js/rebound.js
+++ js/rebound.js
@@ -1,5 +1,9 @@
 //variables to hold DOM
 var playingArea;
+var ball;
+var paddle;
+var score;
+
 
 
 //variables to hold configured Information
@@ -7,9 +11,15 @@ var aWidth; //to hold available width of the screen
 var aHeight; // to hold the available height of the screen
 var pWidth;//playing area width
 var pHeight; // playing area height
-
-
-
+var dx = 2; // to hold horizontal moving speed
+var dy = 1; //to hold vertical moving speed
+var pdx = 48; //to hold paddle moving speed
+var currentScore = 0;
+var timer;
+var paddleLeft = 228;
+var ballLeft = 100;
+var ballTop = 8;
+var drag = false;
 
 //AddEventListener
 window.addEventListener('load',init);
@@ -18,16 +28,248 @@ window.addEventListener('resize',init);
 //functions definions
 function init(){
     playingArea = document.getElementById('playingArea');
+    ball = document.getElementById('ball');
+    paddle = document.getElementById('paddle');
+    score = document.getElementById('score');
+
 
     //calls layoutPage() function
     layoutPage();
+
+
+    //keyboard event listener using function
+    //document.addEventListener('keydown', keyListener, false);
+
+    //keyboard event listener using Arrow Function
+    document.addEventListener('keydown', e =>{
+        var key = e.keyCode;
+        if((key == 37 && paddleLeft > 0)){
+            paddleLeft -= pdx;
+            if(paddleLeft < 0){
+                paddleLeft = 0;
+            }
+        }
+        else if((key == 39 && paddleLeft < (pWidth - 64)  )){
+            paddleLeft += pdx;
+            if(paddleLeft > (pWidth - 64)){
+                paddleLeft = pWidth - 64;
+            }           
+        }
+        paddle.style.left = paddleLeft + 'px';
+    },false);
+    
+    //mouse down event listener using normal functions
+    playingArea.addEventListener('mousedown',mouseDown,false);
+    playingArea.addEventListener('mouseup',mouseUp,false);
+    playingArea.addEventListener('mousemove',mouseMove,false);
+
+    //touch event
+    playingArea.addEventListener('toudhdown',mouseDown,false);
+    playingArea.addEventListener('touchup',mouseUp,false);
+    playingArea.addEventListener('touchmove',mouseMove,false);
+    
+
+
+    document.addEventListener
+    //calls function setBallPosition
+    setBallStartPosition();
+    //start game animation
+    timer = window.requestAnimationFrame(startGame);
 }
 
+function setBallStartPosition(){
+    ballLeft = pWidth /2;
+    ballTop = 8;
+    ball.style.left = ballLeft + 'px';
+    ball.style.top = ballTop + 'px';
+
+    //function call to generate x direction value
+    dx = generateDirectionX(-3,3);
+    //Generates the horizontal moving direction value in the range of -2 to + 2 randomly
+    // var startRange = -2;
+    // var endRange = 2;
+    // dx = Math.round( (Math.random() * (endRange - startRange) + startRange)) ;
+    // console.log("dx: " + dx);
+}
+
+function generateDirectionX(min,max){
+   return Math.round( (Math.random() * (max - min) + min)) ;
+}
 function layoutPage(){
+
     aWidth = window.innerWidth;
     aHeight = window.innerHeight;
     pWidth = aWidth - 20;
     pHeight = aHeight - 20;
     playingArea.style.width = pWidth + 'px';
     playingArea.style.height = pHeight + 'px';
+
+    console.log("pWidth: " + pWidth + " , pHeight : " + pHeight);
+    var scoreHeight = score.clientHeight;
+    var scoreHeight = score.clientHeight;
+    console.log("score Height: " + scoreHeight);
+}
+
+//function to start animation
+function startGame(){
+    render();
+    detectCollisions();
+    timer = window.requestAnimationFrame(startGame);
+
+    // if(ballTop < pHeight ){
+    //     timer = requestAnimationFrame(startGame);
+    // }else{
+    //     gameOver();
+    // }
+
+    let heightToReduce = score.clientHeight -16;
+    if(ballTop > pHeight - heightToReduce){
+        gameOver();
+    }
+    //console.log("Ball Top: " + ballTop + ", pHeight : " + pHeight);
+}
+//function to render animation
+function render(){
+   moveBall();
+   updateScore();
+   difficulty();
+}
+//function to move ball
+function moveBall(){
+    ballLeft += dx;
+    ballTop += dy;
+    ball.style.left = ballLeft + 'px';
+    ball.style.top = ballTop + 'px';
+
+    //console.log("Ball Left : " + ballLeft + " , Ball Top: " + ballTop);
+}
+
+//function to detect collision of ball 
+function detectCollisions(){
+    if(collisionX()){
+        dx *= -1;
+    }
+    if(collisionY()){
+        dy *= -1;
+    }
+}
+
+//function to detect left or right side collision
+function collisionX(){
+    if(ballLeft < 4 || ballLeft > pWidth -20){
+        return true;
+    }
+    return false;
+}
+
+//function to detect bottom side collision
+function collisionY(){
+    
+    if(ballTop < 0){
+        return true;
+    }
+   
+    if(ballTop > (pHeight -64) ){
+        /*
+        if(ballLeft >= paddleLeft && ballLeft <= paddleLeft + 64){
+            dx = generateDirectionX(-8,8);
+            return true;
+        }
+        */
+ 
+        if(ballLeft >= paddleLeft + 16 && ballLeft < paddleLeft + 48){
+            if( dx < 0){
+                dx = -2;
+            }
+            else{
+                dx = 2;
+            }
+            return true;
+        }
+        else if(ballLeft >= paddleLeft && ballLeft < paddleLeft + 16){
+            if( dx < 0){
+                dx = -8;
+            }
+            else{
+                dx = 8;
+            }
+            return true;
+        }
+        else if(ball >= paddleLeft + 48 && ballLeft <= paddleLeft + 64){
+            if( dx < 0){
+                dx = -8;
+            }
+            else{
+                dx = 8;
+            }   
+            return true;
+        }
+    }
+    //console.log("ball Top: " + ballTop);
+    return false;   
+}
+
+function keyListener(e){
+    var key = e.keyCode;
+
+    if((key == 37 && paddleLeft > 0)){
+        paddleLeft -= pdx;
+        if(paddleLeft < 0){
+            paddleLeft = 0;
+        }
+    }
+    else if((key == 39 && paddleLeft < (pWidth - 64)  )){
+        paddleLeft += pdx;
+        if(paddleLeft > (pWidth - 64)){
+            paddleLeft = pWidth - 64;
+        }   
+    }
+    paddle.style.left = paddleLeft + 'px';
+    //console.log("paddleLeft : " + paddleLeft);
+    //console.log("PWidth : " + pWidth);
+}
+
+//mouse events handler functions
+function mouseDown(e){
+    drag = true;
+}
+
+function mouseUp(e){ 
+    drag = false;
+}
+function mouseMove(e){
+    if(drag){
+        //console.log("mouse move");
+        e.preventDefault();
+        paddleLeft = e.clientX -32 || e.targetTouches[0].pageX -32;
+        if(paddleLeft < 0){
+            paddleLeft = 0;
+        }
+        if(paddleLeft > (pWidth - 64)){
+            paddleLeft = (pWidth - 64);
+        }
+        paddle.style.left = paddleLeft + 'px';
+    } 
+}
+
+function difficulty(){
+    if(currentScore % 1000 === 0){
+        if( dy > 0){
+            dy += 2;
+        }
+        else{
+            dy -= 2;
+        }
+    }
+}
+function updateScore(){
+    currentScore += 5;
+    score.innerHTML = "Score : " +  currentScore;
+}
+
+function gameOver(){
+    cancelAnimationFrame(timer);
+    score.innerHTML += "  Game Over!";
+    score.style.backgroundColor = 'rgb(255,0,0)';
+    ball.style.display = "none";
 }
