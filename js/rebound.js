d0af7961d10d70732551a2b30e28756626a71932
Shankar Ghimire <ssghimire@gmail.com>
2020-08-26 16:59:51 -0400

    Completes the first version of the game!

--- js/rebound.js
+++ js/rebound.js
@@ -3,8 +3,13 @@ var playingArea;
 var ball;
 var paddle;
 var score;
-
-
+var gear;
+var controls;
+var newButton;
+var difficultySelect;
+var doneButton;
+var snd;
+var music;
 
 //variables to hold configured Information
 var aWidth; //to hold available width of the screen
@@ -20,6 +25,14 @@ var paddleLeft = 228;
 var ballLeft = 100;
 var ballTop = 8;
 var drag = false;
+var sndEnabled = false;
+var musicEnabled = false;
+
+var beepX;
+var beepY;
+var beepPaddle;
+var beepGameOver;
+var bgMusic;
 
 //AddEventListener
 window.addEventListener('load',init);
@@ -31,11 +44,16 @@ function init(){
     ball = document.getElementById('ball');
     paddle = document.getElementById('paddle');
     score = document.getElementById('score');
-
+    gear = document.getElementById('gear');
+    controls = document.getElementById('controls');
+    newButton = document.getElementById('new');
+    difficultySelect = document.getElementById('difficulty');
+    doneButton = document.getElementById('done');
+    snd = document.getElementById('snd');
+    music = document.getElementById('music');
 
     //calls layoutPage() function
     layoutPage();
-
 
     //keyboard event listener using function
     //document.addEventListener('keydown', keyListener, false);
@@ -68,9 +86,17 @@ function init(){
     playingArea.addEventListener('touchup',mouseUp,false);
     playingArea.addEventListener('touchmove',mouseMove,false);
     
+    //gear controls
+    gear.addEventListener('click',showSettings,false);
+    newButton.addEventListener('click',newGame,false);
+    doneButton.addEventListener('click',hideSettings,false);
+    difficultySelect.addEventListener('change',function(){
+        setDifficulty(difficultySelect.selectedIndex)
+    },false);
+    snd.addEventListener('click',toggleSound,false);
+    music.addEventListener('click',toggleMusic,false);
 
-
-    document.addEventListener
+    
     //calls function setBallPosition
     setBallStartPosition();
     //start game animation
@@ -114,25 +140,20 @@ function layoutPage(){
 function startGame(){
     render();
     detectCollisions();
-    timer = window.requestAnimationFrame(startGame);
+    //timer = window.requestAnimationFrame(startGame);
 
-    // if(ballTop < pHeight ){
-    //     timer = requestAnimationFrame(startGame);
-    // }else{
-    //     gameOver();
-    // }
-
-    let heightToReduce = score.clientHeight -16;
-    if(ballTop > pHeight - heightToReduce){
+    if(ballTop < (pHeight -36) ){
+        //alert("ballTop: " + ballTop + " , pHeight : " + pHeight);
+        timer = requestAnimationFrame(startGame);
+    }else{
         gameOver();
     }
-    //console.log("Ball Top: " + ballTop + ", pHeight : " + pHeight);
 }
 //function to render animation
 function render(){
-   moveBall();
-   updateScore();
-   difficulty();
+    moveBall();   
+    updateScore();
+    difficulty();
 }
 //function to move ball
 function moveBall(){
@@ -140,7 +161,6 @@ function moveBall(){
     ballTop += dy;
     ball.style.left = ballLeft + 'px';
     ball.style.top = ballTop + 'px';
-
     //console.log("Ball Left : " + ballLeft + " , Ball Top: " + ballTop);
 }
 
@@ -157,6 +177,7 @@ function detectCollisions(){
 //function to detect left or right side collision
 function collisionX(){
     if(ballLeft < 4 || ballLeft > pWidth -20){
+        playSound(beepX);
         return true;
     }
     return false;
@@ -166,6 +187,7 @@ function collisionX(){
 function collisionY(){
     
     if(ballTop < 0){
+        playSound(beepY);
         return true;
     }
    
@@ -178,30 +200,36 @@ function collisionY(){
         */
  
         if(ballLeft >= paddleLeft + 16 && ballLeft < paddleLeft + 48){
+           
             if( dx < 0){
                 dx = -2;
             }
             else{
                 dx = 2;
             }
+            playSound(beepPaddle);
             return true;
         }
         else if(ballLeft >= paddleLeft && ballLeft < paddleLeft + 16){
+            
             if( dx < 0){
                 dx = -8;
             }
             else{
                 dx = 8;
             }
+            playSound(beepPaddle);
             return true;
         }
         else if(ball >= paddleLeft + 48 && ballLeft <= paddleLeft + 64){
+       
             if( dx < 0){
                 dx = -8;
             }
             else{
                 dx = 8;
-            }   
+            } 
+            playSound(beepPaddle);  
             return true;
         }
     }
@@ -264,7 +292,116 @@ function difficulty(){
 }
 function updateScore(){
     currentScore += 5;
-    score.innerHTML = "Score : " +  currentScore;
+    score.innerHTML = "Score : " +  currentScore;  
+}
+
+
+function showSettings(){
+    controls.style.display = 'block';
+    cancelAnimationFrame(timer);
+}
+function hideSettings(){
+    controls.style.display = 'none';
+    timer = requestAnimationFrame(startGame);
+}
+
+function setDifficulty(diff){
+    switch( diff ){
+        case 0:
+            dy = 2;
+            pdx = 48;
+            break;
+        case 1:
+            dy = 4;
+            pdx = 32;
+            break;
+        case 2:
+            dy = 6;
+            pdx = 16;
+            break;
+        default:
+            dy = 2;
+            pdx = 48;
+    }
+}
+
+function newGame(){
+    ball.style.display = "block";
+    setBallStartPosition();
+    // ballTop = 8;
+    currentScore = 0;
+    dx = 2;
+    setDifficulty(difficultySelect.selectedIndex);
+    //alert("difficulty level: " + difficultySelect.selectedIndex );
+    score.style.backgroundColor = 'rgb(32,128,64)';
+    hideSettings();
+}
+
+function toggleSound(){
+    //alert("testing");
+    if(beepX == null){
+        initAudio();
+    }
+    sndEnabled = !sndEnabled;
+}
+function playSound(objSound){
+    if(sndEnabled){
+        objSound.play();
+    }
+}
+
+function toggleMusic(){
+    //alert("testing music checkbox");
+    if(bgMusic == null){
+        initAudio();
+    }
+    if(musicEnabled){
+        bgMusic.pause();
+    }else{
+        bgMusic.loop = true;
+        bgMusic.play();
+    }
+    musicEnabled = !musicEnabled;
+    
+}
+
+function initAudio(){
+    //load audio files
+    beepX = new Audio('../sounds/beepX.mp3');
+    beepY = new Audio('../sounds/beepY.mp3');
+    beepPaddle = new Audio('../sounds/beepPaddle.mp3');
+    beepGameOver = new Audio('../sounds/beepGameOver.mp3');
+    bgMusic = new Audio('../sounds/music.mp3');
+    
+
+    //turn off volume
+    beepX.volume = 0;
+    beepY.volume = 0;
+    beepPaddle.volume = 0;
+    beepGameOver.volume = 0;
+    bgMusic.volume = 0;
+
+    //play each file
+    beepX.play();
+    beepY.play();
+    beepPaddle.play();
+    beepGameOver.play();
+    bgMusic.play();
+
+    //pauses each file
+    //this stores them in memory for later
+    beepX.pause();
+    beepY.pause();
+    beepPaddle.pause();
+    beepGameOver.pause();
+    bgMusic.pause();
+
+    //set the volume
+    beepX.volume = 1;
+    beepY.volume = 1;
+    beepPaddle.volume = 1;
+    beepGameOver.volume = 1;
+    bgMusic.volume = 1;
 }
 
 function gameOver(){
@@ -272,4 +409,5 @@ function gameOver(){
     score.innerHTML += "  Game Over!";
     score.style.backgroundColor = 'rgb(255,0,0)';
     ball.style.display = "none";
+    playSound(beepPaddle);
 }
